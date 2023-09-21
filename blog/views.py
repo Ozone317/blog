b333d5b13b6bfa6c1d7dd0e2ad4a5b8f81a0c533
Daksh <dakshkant8@gmail.com>
2023-09-21 20:30:00 +0530

    added all posts page with style improvements

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ def starting_page(request):
     return render(request, "blog/index.html")
 
 def posts(request):
-    pass
+    return render(request, "blog/all_posts.html")
 
 def post_details(requst):
     pass
