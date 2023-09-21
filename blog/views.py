4950bed132b3fe65cf282ceb5ab0c2612adc9530
Daksh <dakshkant8@gmail.com>
2023-09-21 17:26:11 +0530

    basic-views-setup

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 
 # Create your views here.
+
+def starting_page(request):
+    pass
+
+def posts(request):
+    pass
+
+def post_details(requst):
+    pass
