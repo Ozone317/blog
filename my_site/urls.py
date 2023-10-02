1e47ee7ffd63a6ac4b3bd7634f18eae91c7cade5
Daksh <dakshkant8@gmail.com>
2023-10-02 14:45:22 +0530

    fixed static file serving

--- my_site/urls.py
+++ my_site/urls.py
@@ -18,11 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from my_site.settings import STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=STATIC_ROOT)
+] + staticfiles_urlpatterns()
